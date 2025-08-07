cfe8b03e75d6a980e635a66c6f5610c9c9c0ba8d
황승재 <joyfull223@naver.com>
2025-08-07 16:21:27 +0900

    연습

---
+++ src/Header/Header.jsx
@@ -0,0 +1,44 @@
+import "./Header.css";
+import Bell from '../assets/bell.png';
+import Search from '../assets/search.png';
+import User from '../assets/user-round.png';
+import Logo from '../assets/logo.png';
+import { Link, useLocation } from "react-router-dom";
+
+export default function Header() {
+  const location = useLocation();
+
+  const menuList = [
+    { name: "메인", path: "/" },
+    { name: "탐색", path: "/explore" },
+    { name: "AI 검색", path: "/aisearch" },
+  ];
+
+  return (
+    <header className="custom-header">
+      <img src={Logo} alt="서산에 뭐 issue?" className="header-logo" />
+
+      <nav className="header-nav">
+        {menuList.map(menu => (
+          <div className="header-menu-wrap" key={menu.path}>
+            <Link
+              to={menu.path}
+              className="header-nav-link"
+            >
+              {menu.name}
+            </Link>
+            {location.pathname === menu.path && (
+              <div className="header-underline"></div>
+            )}
+          </div>
+        ))}
+      </nav>
+
+      <div className="header-icons">
+        <img src={Bell} alt="알림" className="header-icon" />
+        <img src={Search} alt="검색" className="header-icon" />
+        <img src={User} alt="유저" className="header-icon" />
+      </div>
+    </header>
+  );
+}
