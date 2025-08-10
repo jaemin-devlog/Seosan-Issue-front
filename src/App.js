7ddbc1cb79392610a1fd2be368708e9c8fc757d2
ym0506 <atat9828@naver.com>
2025-08-11 00:57:43 +0900

    탐색 수정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './Header/Header';
 import Mainpage from './Mainpage/Mainpage';
 import AiSearch from "./AiSearch/AiSearch";
-import Weather from "./Weather/Weather"; 
+import Weather from "./Weather/Weather";
+import Explore from "./Explore/Explore"; 
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/AiSearch" element={<AiSearch />} />
+        <Route path="/Explore" element={<Explore />} />
       </Routes>
     </Router>
   );
